eea20f07dc210c52474b56ef24002eb9a8f0c198
Isma <didouhismael@gmail.com>
2020-05-19 22:14:42 +0200

    Clean main app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // libs
-const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
+
+// routes
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 // config
 const port = 8080;
@@ -21,147 +24,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// route handlers
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: { tours },
-  });
-};
-
-const getTour = (req, res) => {
-  // Find by ID
-  const id = parseInt(req.params.id, 10); // To decimal
-  const tour = tours.find((item) => item.id === id);
-
-  if (tour === undefined) {
-    return res.status(404).json({
-      status: "fail",
-      message: "not_found",
-      params: req.params,
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    params: req.params,
-    tour,
-  });
-};
-
-const createTour = (req, res) => {
-  const id = tours[tours.length - 1].id + 1;
-  // New object with the ID and the data from the request
-  const tour = { id, ...req.body };
-
-  tours.push(tour);
-
-  // Always async, as we are inside a callback function and we never want to block the event loop
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours, null, "\t"), // Pretty output
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        created_at: req.requestTime,
-        data: {
-          tour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  // Find by ID
-  const id = parseInt(req.params.id, 10); // To decimal
-  const tour = tours.find((item) => item.id === id);
-
-  if (tour === undefined) {
-    return res.status(404).json({
-      status: "fail",
-      message: "not_found",
-      params: req.params,
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour>",
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  // Find by ID
-  const id = parseInt(req.params.id, 10); // To decimal
-  const tour = tours.find((item) => item.id === id);
-
-  if (tour === undefined) {
-    return res.status(404).json({
-      status: "fail",
-      message: "not_found",
-      params: req.params,
-    });
-  }
-
-  // It won't return anything, as we are using 204
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(501).json({
-    status: "error",
-    message: "Feature not implemented yet",
-  });
-};
-const createUser = (req, res) => {
-  res.status(501).json({
-    status: "error",
-    message: "Feature not implemented yet",
-  });
-};
-const getUser = (req, res) => {
-  res.status(501).json({
-    status: "error",
-    message: "Feature not implemented yet",
-  });
-};
-const updateUser = (req, res) => {
-  res.status(501).json({
-    status: "error",
-    message: "Feature not implemented yet",
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(501).json({
-    status: "error",
-    message: "Feature not implemented yet",
-  });
-};
-
-/** endpoints */
-
-// routers
-const tourRouter = express.Router();
-const userRouter = express.Router();
-// tour routes
-tourRouter.route("/").get(getAllTours).post(createTour);
-tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
-
-// user routes
-userRouter.route("/").get(getAllUsers).post(createUser);
-userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 // mount routers
 app.use("/api/v1/tours", tourRouter);
