d48408ff161c53aa6e0365d067eb638665affc95
Isma <didouhismael@gmail.com>
2020-05-19 23:11:02 +0200

    Remove test middleware

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
