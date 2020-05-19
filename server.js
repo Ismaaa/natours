14018334250af88b1722c20a41ae11eb0bedc78d
Isma <didouhismael@gmail.com>
2020-05-19 23:30:34 +0200

    Added dotenv + vars into config.env file

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+// libs
+const dotenv = require("dotenv");
+
 // modules
 const app = require("./app");
 
 // config
-const port = 8080;
+dotenv.config({ path: "./config.env" });
+const port = process.env.PORT;
 
 // start server
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+console.log(process.env);
