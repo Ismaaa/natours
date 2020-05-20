5c9851d400fb5ad5aa74433d0991a28326bb47c4
Isma <didouhismael@gmail.com>
2020-05-21 00:17:33 +0200

    enable dotenv

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
 // // libs
 const functions = require('firebase-functions');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
 // modules
 const app = require('./app');
 
 // config
-// dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config.env' });
 
 // start server with firebase functions
 exports.app = functions.https.onRequest(app);
