4f438cb6fd4e2ada171ed64b53b6c13d7b1219e8
Isma <didouhismael@gmail.com>
2020-05-21 21:04:34 +0200

    tour database schema

--- functions/index.js
+++ functions/index.js
@@ -24,5 +24,21 @@ mongoose
     console.log('DB CONNECTION SUCCESSFUL !');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
 // start server with firebase functions
 exports.app = functions.https.onRequest(app);
