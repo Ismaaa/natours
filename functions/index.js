fb85d8a2a186a3772d360464494cf2414f1612ce
Isma <didouhismael@gmail.com>
2020-05-21 20:59:17 +0200

    setup database connection

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 // // libs
+const mongoose = require('mongoose');
 const functions = require('firebase-functions');
 const dotenv = require('dotenv');
 
@@ -7,6 +8,21 @@ const app = require('./app');
 
 // config
 dotenv.config({ path: './config.env' });
+const DATABASE_CONNECTION = process.env.DATABASE_HOST.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+// database connection
+mongoose
+  .connect(DATABASE_CONNECTION, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB CONNECTION SUCCESSFUL !');
+  });
 
 // start server with firebase functions
 exports.app = functions.https.onRequest(app);
