73d15f3e419db126b318b40b12de9ff4e435fa5a
Isma <didouhismael@gmail.com>
2020-05-21 22:37:24 +0200

    [Tour] - Extracted modal logic into a new model file

--- functions/index.js
+++ functions/index.js
@@ -24,23 +24,5 @@ mongoose
     console.log('DB CONNECTION SUCCESSFUL !');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
 // start server with firebase functions
 exports.app = functions.https.onRequest(app);
