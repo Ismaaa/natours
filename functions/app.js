2c8d63a62147a4df2c4eca5bbe685a99423e611b
Isma <didouhismael@gmail.com>
2020-05-26 23:39:46 +0200

    morgan logging

--- functions/app.js
+++ functions/app.js
@@ -10,7 +10,7 @@ const userRouter = require('./routes/UserRoutes');
 const app = express();
 
 // Middlewares
-// app.use(morgan('combined'));
+app.use(morgan('tiny'));
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
