2ab114972ce2e881df13775609157c242fa41eba
Isma <didouhismael@gmail.com>
2020-05-26 23:37:02 +0200

    404 response

--- functions/app.js
+++ functions/app.js
@@ -10,9 +10,7 @@ const userRouter = require('./routes/UserRoutes');
 const app = express();
 
 // Middlewares
-if (process.env.NODE_ENV === 'dev') {
-  app.use(morgan('dev'));
-}
+// app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -26,5 +24,13 @@ app.use((req, res, next) => {
 // mount routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// 404 response
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl}`,
+  });
+});
 
 module.exports = app;
