5cf9eeb3646caa301d06033d39c77aeb74b2be48
Isma <didouhismael@gmail.com>
2020-05-26 23:49:06 +0200

    main error handling middleware

--- functions/app.js
+++ functions/app.js
@@ -26,11 +26,25 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // 404 response
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`,
+app.all('*', (req, res, next) => {
+  const error = new Error(`Can't find ${req.originalUrl}`);
+  error.status = 'fail';
+  error.statusCode = 404;
+
+  // If we pass a param, express will know that it's an error
+  // and it will skipp all next middlewares and go directly
+  // to the error handler
+  next(error);
+});
+
+// error handling (if we pass 4 params
+// express will know that it's an error handler)
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500).json({
+    status: error.status || 'fail',
+    message: error.message,
   });
+  next();
 });
 
 module.exports = app;
